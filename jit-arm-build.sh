788199e59dffdc38ff3b23cc1f2544fd1549f8d0
Luca Succi <luca.succi@stritzinger.com>
2025-07-29 18:16:30 +0200

    Add update preloaded step to build script

--- jit-arm-build.sh
+++ jit-arm-build.sh
@@ -1,6 +1,7 @@
-cd otp
+cd otp && \
 ./otp_build update_configure --no-commit && \
 ./otp_build configure --xcomp-conf=xcomp/erl-xcomp-arm-linux-custom.conf && \
+./otp_build update_preloaded --no-commit && \
 ./otp_build boot -a && \
 rm -rf RELEASE/* && \
 mkdir -p RELEASE && \
