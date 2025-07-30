d277ff343d193345bf406e59ba899f1723b33aeb
Luca Succi <luca.succi@stritzinger.com>
2025-07-30 12:27:26 +0200

    Set BINDIR variable to avoid error on boot

--- run_debug.sh
+++ run_debug.sh
@@ -1,1 +1,7 @@
-qemu-arm -L /usr/arm-linux-gnueabihf -g 1234 ./otp/RELEASE/erts-15.0/bin/beam.smp -JDdump true -JMsingle true -- -root ./otp/RELEASE -bindir ./otp/RELEASE/erts-15.0/bin -progname erl -home /home/vagrant
+#!/bin/bash
+
+# Set the BINDIR environment variable to point to the Erlang binaries
+
+export BINDIR=/home/vagrant/arm32-jit/otp/RELEASE/erts-15.0/bin
+
+qemu-arm -L /usr/arm-linux-gnueabihf -g 1234 ./otp/RELEASE/erts-15.0/bin/beam.smp -JDdump true -JMsingle true -- -root /home/vagrant/arm32-jit/otp/RELEASE -progname erl -home /home/vagrant
