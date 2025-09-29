7a379523e0de32aeaf7614d29e5368d8dc930da9
Luca Succi <luca.succi@stritzinger.com>
2025-09-29 14:36:25 +0200

    Add verbosity flag to the run script

--- run_debug.sh
+++ run_debug.sh
@@ -4,4 +4,4 @@
 
 export BINDIR=/home/vagrant/arm32-jit/otp/RELEASE/erts-15.0/bin
 
-qemu-arm -L /usr/arm-linux-gnueabihf -g 1234 ./otp/RELEASE/erts-15.0/bin/beam.smp -JDdump true -JMsingle true -- -root /home/vagrant/arm32-jit/otp/RELEASE -progname erl -home /home/vagrant
+qemu-arm -L /usr/arm-linux-gnueabihf -g 1234 ./otp/RELEASE/erts-15.0/bin/beam.smp -v -S 1:1 -SDcpu 1:1 -SDio 1 -JDdump true -JMsingle true -- -root /home/vagrant/arm32-jit/otp/RELEASE -progname erl -home /home/vagrant
